c2396ef056993e4f6a54fbd60517425e5b4d5600
saatvik-10 <saatvikmadan529@gmail.com>
2025-01-11 22:18:34 +0530

    updated lint, db, added clerk neon connection

---
+++ src/app/sync-user/page.tsx
@@ -0,0 +1,39 @@
+import { db } from "@/server/db";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { notFound, redirect } from "next/navigation";
+
+const SyncUser = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("No user found");
+  }
+
+  const client = await clerkClient();
+  const user = await client.users.getUser(userId);
+
+  if (!user.emailAddresses[0]?.emailAddress) {
+    return notFound();
+  }
+
+  //if user exists, update it
+  await db.user.upsert({
+    where: {
+      emailAddress: user.emailAddresses[0].emailAddress,
+    },
+    update: {
+      imageUrl: user.imageUrl,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    },
+    create: {
+      id: user.id,
+      emailAddress: user.emailAddresses[0].emailAddress,
+      imageUrl: user.imageUrl,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    },
+  });
+  return redirect("/dashboard");
+};
+
+export default SyncUser;
