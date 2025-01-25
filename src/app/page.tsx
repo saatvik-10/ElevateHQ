b74bb6c050cec66de5559e3696739ab979bc5c7b
saatvik-10 <saatvikmadan529@gmail.com>
2025-01-25 17:42:12 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-"use client";
+// "use client";
 
-import React, { useEffect } from "react";
-import { useRouter } from "next/navigation";
+// import React, { useEffect } from "react";
+// import { useRouter } from "next/navigation";
 
-export default function Home() {
-  const router = useRouter();
+// export default function Home() {
+//   const router = useRouter();
 
-  useEffect(() => {
-    router.push("/sync-user");
-  }, [router]);
+//   useEffect(() => {
+//     router.push("/sync-user");
+//   }, [router]);
 
-  return null;
-}
+//   return null;
+// }
