1eef4487ed266829008b17abe2f9154161be5ef2
saatvik-10 <saatvikmadan529@gmail.com>
2025-01-25 12:41:48 +0530

    added billing page

---
+++ src/app/(protected)/billing/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Billing = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <h1 className="text-bold font-5xl">Coming Soon!</h1>
+    </div>
+  );
+};
+
+export default Billing;
