c9cc26e3b8fca242094829dbf57c0fce746babc6
saatvik-10 <saatvikmadan529@gmail.com>
2025-01-25 12:38:20 +0530

    fixxes in ui and sync-user route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-export default async function Home() {
-  return <h1>Hello World</h1>;
+"use client";
+
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/sync-user");
+  }, [router]);
+
+  return null;
 }
